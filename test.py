27aa1d2b155c153067d18a020c98fb1e8ecff037
sachin <sysachin349@yahoo.com>
2023-12-19 17:39:02 +0530

    test

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 with open('test.txt','a') as file:
-    file.write(f'This is file generated from jenkin  \n')
-    file.write(f"new message")
+    file.write(f'This is file generated from jenkin, at {datetime.now()}  \n')
+    
