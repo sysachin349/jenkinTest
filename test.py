0938f24f32840860b1710dc3117bc2e75065d205
sachin <sysachin349@yahoo.com>
2023-12-19 17:45:12 +0530

    test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 
-with open('test.txt','a') as file:
+with open('test1.txt','a') as file:
     file.write(f'This is file generated from jenkin, at {datetime.now()}  \n')
     
